5620cc03bb7853878aa2fbd45ac43da423b09252
asnakeassefa <asnakeassefa28@gmail.com>
2022-10-24 23:56:37 +0300

    power of n

---
+++ powerOfn.py
@@ -0,0 +1,8 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+#         a = sys.getrecursionlimit()
+        
+#         if n > a:
+#             return float(0)
+        
+        return x ** n
